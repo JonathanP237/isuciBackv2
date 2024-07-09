0cb3a6357b0b23cbd2a5bfd7f7e070436a2529e3
Jaaav08 <jopatinoc@udistrital.edu.co>
2024-07-09 17:16:59 -0500

    jeffryILoveU

---
+++ controllers/userController.js
@@ -0,0 +1,17 @@
+import { getCiclistasLibresByGeneroAndEspecialidad } from '../services/userService.js';
+
+export const getCiclistasLibres = async (req, res) => {
+  const { genero, especialidad } = req.query;
+
+  if (!genero || !especialidad) {
+    return res.status(400).json({ message: "Falta el genero o la especialidad en la solicitud." });
+  }
+
+  try {
+    const ciclistasLibres = await getCiclistasLibresByGeneroAndEspecialidad(genero, especialidad);
+    res.json(ciclistasLibres);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error interno del servidor." });
+  }
+};
