4cd257beb401d7f23e05f83615be00ef6628b6f9
Jonathan <japuentess@udistrital.edu.co>
2024-07-14 01:39:33 -0500

    Se corrige error de retorno

--- services/squadService.js
+++ services/squadService.js
@@ -3,5 +3,5 @@ import pool from '../config/dbConfig.js';
 export async function registrarEscuadra(id,desc,idPais) {
     const sql = `insert into escuadras (idescuadra, desescuadra, tiempoescuadra, pais) values ($1, $2, $3, $4)`;
     const result = await pool.query(sql, [id,desc,0,idPais]);
-    return result.json({ message: "Escuadra creada" });
+    return { message: "Escuadra creada" };
 }
