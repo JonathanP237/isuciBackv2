3e6eb39e23c62c2f7e433e2d457bec272cb7781c
Jonathan <japuentess@udistrital.edu.co>
2024-07-14 01:14:15 -0500

    Corrección error ruta

--- controllers/squadController.js
+++ controllers/squadController.js
@@ -1,12 +1,12 @@
 import {registrarEscuadra} from '../services/squadService.js';
 
 export const registroEscuadra = async (req, res) => {
-  const { id, desc, idPais } = req.body;
-  if (!id || !desc || !idPais) {
+  const { id, desc, pais } = req.body;
+  if (!id || !desc || !pais) {
     return res.status(400).json({ message: "Falta el id, la descripcion o el id del pais en la solicitud." });
   }
   try{
-    const result = await registrarEscuadra(id,desc,idPais);
+    const result = await registrarEscuadra(id,desc,pais);
     res.json(result);
   }catch(error){
     console.error(error);
--- routes/squadRoutes.js
+++ routes/squadRoutes.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import { crearEscuadra } from '../controllers/squadController.js';
 import { registroEscuadra } from '../controllers/squadController.js';
 
 const router = express.Router();
 
-router.post('/crear-escuadra', crearEscuadra);
 router.post('/registro-escuadra', registroEscuadra);
 
 export default router;
