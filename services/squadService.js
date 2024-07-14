7a2829849faf50631c26dc8be7aea4f888a9e4d0
Jonathan <japuentess@udistrital.edu.co>
2024-07-14 01:17:07 -0500

    Correción nombre de la tabla

--- services/squadService.js
+++ services/squadService.js
@@ -1,7 +1,7 @@
 import pool from '../config/dbConfig.js';
 
 export async function registrarEscuadra(id,desc,idPais) {
-    const sql = `insert into escuadra (idescuadra, desescuadra, tiempoescuadra, pais) values ($1, $2, $3, $4)`;
+    const sql = `insert into escuadras (idescuadra, desescuadra, tiempoescuadra, pais) values ($1, $2, $3, $4)`;
     const result = await pool.query(sql, [id,desc,0,idPais]);
     return result.json({ message: "Escuadra creada" });
 }
