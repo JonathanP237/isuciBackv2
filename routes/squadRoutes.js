1b83cff2db6c0efe9cb4a44e71577fdae81aa4be
Jonathan <japuentess@udistrital.edu.co>
2024-07-14 13:06:54 -0500

    Se añade ruta para endpoint de asignar miembros a escuadra

--- routes/squadRoutes.js
+++ routes/squadRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { registroEscuadra, getEscuadras, getMiembrosEscuadra } from '../controllers/squadController.js';
+import { registroEscuadra, getEscuadras, getMiembrosEscuadras, asignarMiembrosEscuadras } from '../controllers/squadController.js';
 
 const router = express.Router();
 
 router.post('/registro-escuadra', registroEscuadra);
 router.get('/escuadras', getEscuadras);
-router.post('/miembros-escuadra', getMiembrosEscuadra);
+router.post('/miembros-escuadra', getMiembrosEscuadras);
+router.post('/asignar-miembros', asignarMiembrosEscuadras);
 
 export default router;
